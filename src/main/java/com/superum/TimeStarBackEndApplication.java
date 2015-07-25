1e96d577e7a8f75b20199e2792739d481646d007
Goodlike <henrisssss@hotmail.com>
2015-07-25 13:15:23 +0300

    Fixed the c3p0.properties file to actually appear

--- src/main/java/com/superum/TimeStarBackEndApplication.java
+++ src/main/java/com/superum/TimeStarBackEndApplication.java
@@ -1,11 +1,7 @@
 package com.superum;
 
-import javax.servlet.Filter;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.filter.CharacterEncodingFilter;
 
 @SpringBootApplication
 public class TimeStarBackEndApplication {
@@ -13,13 +9,5 @@ public class TimeStarBackEndApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TimeStarBackEndApplication.class, args);
 	}
-	
-	/*@Bean
-    public Filter characterEncodingFilter() {
-        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
-        characterEncodingFilter.setEncoding("UTF-8");
-        characterEncodingFilter.setForceEncoding(true);
-        return characterEncodingFilter;
-    }*/
-	
+
 }
