44d5b830c5fca93161c4d79182b1c62c952a95e0
Goodlike <henrisssss@hotmail.com>
2015-08-07 17:43:06 +0300

    Added support for IllegalFieldValueException

--- src/main/java/com/superum/api/exception/GeneralErrorHandler.java
+++ src/main/java/com/superum/api/exception/GeneralErrorHandler.java
@@ -1,5 +1,6 @@
 package com.superum.api.exception;
 
+import org.joda.time.IllegalFieldValueException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -21,6 +22,11 @@ public class GeneralErrorHandler {
     @ExceptionHandler
     void handleUnauthorizedRequestException(UnauthorizedRequestException e, HttpServletResponse response) throws IOException {
         response.sendError(HttpStatus.UNAUTHORIZED.value(), "Couldn't authenticate user; " + e.getMessage());
+    }
+
+    @ExceptionHandler
+    void handleIllegalFieldValueException(IllegalFieldValueException e, HttpServletResponse response) throws Exception {
+        response.sendError(HttpStatus.BAD_REQUEST.value(), "Invalid date or time field, check your URL/JSON; " + e.getMessage());
     }
 
 }
