38abb75684d6f30b54a6821f959b24957b55e2ce
Goodlike <henrisssss@hotmail.com>
2015-08-18 16:30:27 +0300

    Updated equals to support custom equality checks
    Also, pure equals() check is not also supported

--- src/main/java/com/superum/helper/Equals.java
+++ src/main/java/com/superum/helper/Equals.java
@@ -2,12 +2,15 @@ package com.superum.helper;
 
 import org.jooq.lambda.Seq;
 import org.jooq.lambda.tuple.Tuple;
+import org.jooq.lambda.tuple.Tuple2;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 /**
  * <pre>
@@ -20,23 +23,41 @@ import java.util.function.Function;
 public final class Equals<T> {
 
     /**
+     * This method checks Objects.equals() regardless of type
+     * @return true if all provided getters return equal objects for both t1 and t2, false otherwise
+     */
+    public boolean equalsPure(T t1, T t2) {
+        return equals(t1, t2, tuple2 -> tuple2.map(Objects::equals));
+    }
+
+    /**
+     * This method checks Objects.equals() for all objects, except BigDecimals, which check
+     * SpecialUtils.equalsJavaMathBigDecimal()
      * @return true if all provided getters return equal objects for both t1 and t2, false otherwise
      */
     public boolean equals(T t1, T t2) {
+        return equals(t1, t2, tuple2 -> tuple2.map(Equals::equalsAdjusted));
+    }
+
+    /**
+     * This method uses a provided equality predicate to test if two objects are equal
+     * @return true if all provided getters return equal objects for both t1 and t2, false otherwise
+     */
+    public boolean equals(T t1, T t2, Predicate<Tuple2<Object, Object>> equalityFunction) {
         return !Seq.seq(getters)
                 .map(getter -> Tuple.tuple(getter.apply(t1), getter.apply(t2)))
-                .filter(tuple2 -> !tuple2.map(Equals::equalsAdjusted))
+                .filter(equalityFunction.negate())
                 .findAny().isPresent();
     }
 
     /**
-     * WARNING! BigDecimals can produce inconsistent results for this method
+     * WARNING! BigDecimals/arrays can produce inconsistent results for this method
      * @return true if all provided getters return equal objects for every parameter, false otherwise
      */
     @SafeVarargs
     public final boolean equals(T... objects) {
         return !Seq.seq(getters)
-                .map(getter -> Arrays.stream(objects).map(getter::apply).distinct())
+                .map(getter -> Stream.of(objects).map(getter::apply).distinct())
                 .filter(stream -> stream.count() > 1)
                 .findAny().isPresent();
     }
@@ -60,6 +81,9 @@ public final class Equals<T> {
 
     private final List<Function<T, ?>> getters;
 
+    /**
+     * Ensures that BigDecimals are compared ignoring their scale
+     */
     private static boolean equalsAdjusted(Object o1, Object o2) {
         return o1 instanceof BigDecimal && o2 instanceof BigDecimal
                 ? SpecialUtils.equalsJavaMathBigDecimal((BigDecimal)o1, (BigDecimal)o2)
