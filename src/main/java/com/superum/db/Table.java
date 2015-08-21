dbc207e19cdd270fdbb9e20b91af4e191ff1e91d
Goodlike <henrisssss@hotmail.com>
2015-08-21 16:41:43 +0300

    Added missing tables

--- src/main/java/com/superum/db/Table.java
+++ src/main/java/com/superum/db/Table.java
@@ -5,7 +5,7 @@ import java.util.stream.Stream;
 
 public enum Table {
 
-    teacher, customer, group;
+    teacher, customer, group, student, lesson;
 
     public static Optional<Table> forName(String name) {
         return Stream.of(Table.values()).filter(tables -> tables.name().equals(name)).findAny();
