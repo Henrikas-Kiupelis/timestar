641d58273fccc6060e006833525d9551b3de56a2
Goodlike <henrisssss@hotmail.com>
2015-08-21 13:01:12 +0300

    Exceptions should now print their traces instead of being quiet

--- src/main/java/com/superum/api/exception/GeneralErrorHandler.java
+++ src/main/java/com/superum/api/exception/GeneralErrorHandler.java
@@ -16,16 +16,19 @@ public class GeneralErrorHandler {
 
     @ExceptionHandler
     void handleInvalidRequestException(InvalidRequestException e, HttpServletResponse response) throws IOException {
+        e.printStackTrace();
         response.sendError(HttpStatus.BAD_REQUEST.value(), "Invalid request format, check your URL/JSON; " + e.getMessage());
     }
 
     @ExceptionHandler
     void handleUnauthorizedRequestException(UnauthorizedRequestException e, HttpServletResponse response) throws IOException {
+        e.printStackTrace();
         response.sendError(HttpStatus.UNAUTHORIZED.value(), "You are not authorized to use this request; " + e.getMessage());
     }
 
     @ExceptionHandler
     void handleIllegalFieldValueException(IllegalFieldValueException e, HttpServletResponse response) throws Exception {
+        e.printStackTrace();
         response.sendError(HttpStatus.BAD_REQUEST.value(), "Invalid date or time field, check your URL/JSON; " + e.getMessage());
     }
 
--- src/main/java/com/superum/exception/GenericErrorHandler.java
+++ src/main/java/com/superum/exception/GenericErrorHandler.java
@@ -20,31 +20,37 @@ public class GenericErrorHandler {
 
     @ExceptionHandler
     void handleDatabaseException(DatabaseException e, HttpServletResponse response) throws IOException {
+        e.printStackTrace();
         response.sendError(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Database couldn't return value; " + e.getMessage());
     }
 
     @ExceptionHandler
     void handleDataAccessException(DataAccessException e, HttpServletResponse response) throws Exception {
+        e.printStackTrace();
         response.sendError(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Unexpected error occurred");
     }
 
     @ExceptionHandler
     void handleAssertionError(AssertionError e, HttpServletResponse response) throws Exception {
+        e.printStackTrace();
         response.sendError(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Something has gone terribly wrong. REALLY TERRIBLY!");
     }
 
     @ExceptionHandler
     void handleIllegalStateException(IllegalStateException e, HttpServletResponse response) throws Exception {
+        e.printStackTrace();
         response.sendError(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Something wrong happened in the server");
     }
 
     @ExceptionHandler
     void handleIllegalArgumentException(IllegalArgumentException e, HttpServletResponse response) throws Exception {
+        e.printStackTrace();
         response.sendError(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Someone put something where they shouldn't; " + e.getMessage());
     }
 
     @ExceptionHandler
     void handleNullPointerException(NullPointerException e, HttpServletResponse response) throws Exception {
+        e.printStackTrace();
         response.sendError(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Someone somewhere a null pointer");
     }
 
