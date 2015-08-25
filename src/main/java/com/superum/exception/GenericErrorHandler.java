6df1a1a5bd87bb864ef375706e0c8f221b7b4b37
Goodlike <henrisssss@hotmail.com>
2015-08-25 17:40:19 +0300

    Generic errors will now be logged

--- src/main/java/com/superum/exception/GenericErrorHandler.java
+++ src/main/java/com/superum/exception/GenericErrorHandler.java
@@ -1,5 +1,7 @@
 package com.superum.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -20,38 +22,40 @@ public class GenericErrorHandler {
 
     @ExceptionHandler
     void handleDatabaseException(DatabaseException e, HttpServletResponse response) throws IOException {
-        e.printStackTrace();
+        LOG.error("Database couldn't return value", e);
         response.sendError(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Database couldn't return value; " + e.getMessage());
     }
 
     @ExceptionHandler
     void handleDataAccessException(DataAccessException e, HttpServletResponse response) throws Exception {
-        e.printStackTrace();
+        LOG.error("Unexpected error occurred", e);
         response.sendError(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Unexpected error occurred");
     }
 
     @ExceptionHandler
     void handleAssertionError(AssertionError e, HttpServletResponse response) throws Exception {
-        e.printStackTrace();
+        LOG.error("Something has gone terribly wrong. REALLY TERRIBLY!", e);
         response.sendError(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Something has gone terribly wrong. REALLY TERRIBLY!");
     }
 
     @ExceptionHandler
     void handleIllegalStateException(IllegalStateException e, HttpServletResponse response) throws Exception {
-        e.printStackTrace();
+        LOG.error("Something wrong happened in the server", e);
         response.sendError(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Something wrong happened in the server");
     }
 
     @ExceptionHandler
     void handleIllegalArgumentException(IllegalArgumentException e, HttpServletResponse response) throws Exception {
-        e.printStackTrace();
+        LOG.error("Someone put something where they shouldn't", e);
         response.sendError(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Someone put something where they shouldn't; " + e.getMessage());
     }
 
     @ExceptionHandler
     void handleNullPointerException(NullPointerException e, HttpServletResponse response) throws Exception {
-        e.printStackTrace();
+        LOG.error("Someone somewhere a null pointer", e);
         response.sendError(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Someone somewhere a null pointer");
     }
+
+    private static final Logger LOG = LoggerFactory.getLogger(GenericErrorHandler.class);
 
 }
