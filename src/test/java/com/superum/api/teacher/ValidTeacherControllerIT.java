8b247b94580585a89a783d1080d1081ffdeff051
Goodlike <henrisssss@hotmail.com>
2015-09-03 14:47:50 +0300

    Updated ValidTeacherController tests to use new methods

--- src/test/java/com/superum/api/teacher/ValidTeacherControllerIT.java
+++ src/test/java/com/superum/api/teacher/ValidTeacherControllerIT.java
@@ -1,200 +1,256 @@
 package com.superum.api.teacher;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.superum.env.IntegrationTestEnvironment;
+import com.superum.helper.DB;
+import com.superum.helper.Fake;
+import org.jooq.lambda.Unchecked;
 import org.junit.Test;
 import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
+import java.util.List;
+
+import static com.superum.helper.ResultVariation.*;
+import static com.superum.utils.MockMvcUtils.fromResponse;
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @TransactionConfiguration(defaultRollback = true)
 @Transactional
 public class ValidTeacherControllerIT extends IntegrationTestEnvironment {
 
     @Test
-    public void test() {
-
-    }
-
-    /*@Test
     public void insertingTeacherWithoutId_shouldCreateNewTeacher() throws Exception {
-        FullTeacherDTO teacher = makeFakeFullTeacher(TEACHER_SEED).withoutId();
+        FullTeacherDTO teacher = Fake.teacher(NEW_TEACHER_ID).withoutId();
 
-        MvcResult result = mockMvc.perform(put("/timestar/api/v2/teacher")
-                .contentType(APPLICATION_JSON_UTF8)
-                .header("Authorization", authHeader())
-                .content(convertObjectToJsonBytes(teacher)))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(APPLICATION_JSON_UTF8))
-                .andReturn();
+        FullTeacherDTO insertedTeacher = performPut(DEFAULT_PATH, teacher, OK)
+                .map(Unchecked.function(this::readTeacher))
+                .orElseThrow(() -> new Exception("Successful insertion should return a teacher!"));
 
-        FullTeacherDTO returnedTeacher = fromResponse(result, FullTeacherDTO.class);
-        int teacherId = returnedTeacher.getId();
-        teacher = teacher.withId(teacherId);
+        assertEquals("Inserted teacher should be equal to the original (except id)",
+                teacher.withId(insertedTeacher.getId()), insertedTeacher);
 
-        assertEquals("The returned teacher should be equal to original teacher, except for id field; ", returnedTeacher, teacher);
-
-        FullTeacherDTO teacherFromDB = databaseHelper.readFullTeacherFromDb(teacherId);
-
-        assertEquals("The teacher in the database should be equal to the returned teacher; ", teacherFromDB, returnedTeacher);
+        assertInDatabase(insertedTeacher);
     }
 
     @Test
-    public void readingTeacherWithValidId_shouldReturnATeacher() throws Exception {
-        FullTeacherDTO insertedTeacher = databaseHelper.insertFullTeacherIntoDb(makeFakeFullTeacher(TEACHER_SEED).withoutId());
-        int teacherId = insertedTeacher.getId();
+    public void insertingTeacherWithId_shouldReturn400() throws Exception {
+        FullTeacherDTO teacher = Fake.teacher(NEW_TEACHER_ID);
 
-        MvcResult result = mockMvc.perform(get("/timestar/api/v2/teacher/{teacherId}", teacherId)
-                .contentType(APPLICATION_JSON_UTF8)
-                .header("Authorization", authHeader()))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(APPLICATION_JSON_UTF8))
-                .andReturn();
+        performPut(DEFAULT_PATH, teacher, BAD, status().isBadRequest());
 
-        FullTeacherDTO returnedTeacher = fromResponse(result, FullTeacherDTO.class);
-
-        assertEquals("The read teacher should be equal to original teacher; ", returnedTeacher, insertedTeacher);
-
-        FullTeacherDTO teacherFromDB = databaseHelper.readFullTeacherFromDb(teacherId);
-
-        assertEquals("The teacher in the database should be equal to the returned teacher; ", teacherFromDB, returnedTeacher);
+        assertNotInDatabase(teacher);
     }
 
     @Test
-    public void updatingTeacherWithValidData_shouldReturnOldTeacher() throws Exception {
-        FullTeacherDTO insertedTeacher = databaseHelper.insertFullTeacherIntoDb(makeFakeFullTeacher(TEACHER_SEED).withoutId());
-        int teacherId = insertedTeacher.getId();
+    public void insertingTeacherWithoutName_shouldReturn400() throws Exception {
+        FullTeacherDTO teacher = FullTeacherDTO.builder()
+                .paymentDay(Fake.day(NEW_TEACHER_ID))
+                .hourlyWage(Fake.wage(NEW_TEACHER_ID))
+                .academicWage(Fake.wage(NEW_TEACHER_ID))
+                .surname(Fake.surname(NEW_TEACHER_ID))
+                .phone(Fake.phone(NEW_TEACHER_ID))
+                .city(Fake.city(NEW_TEACHER_ID))
+                .email(Fake.email(NEW_TEACHER_ID))
+                .languages(Fake.language(NEW_TEACHER_ID))
+                .build();
 
-        FullTeacherDTO updatedTeacher = makeFakeFullTeacher(TEACHER_SEED + 1).withId(teacherId);
-
-        mockMvc.perform(post("/timestar/api/v2/teacher")
-                .contentType(APPLICATION_JSON_UTF8)
-                .header("Authorization", authHeader())
-                .content(convertObjectToJsonBytes(updatedTeacher)))
-                .andDo(print())
-                .andExpect(status().isOk());
-
-        FullTeacherDTO teacherFromDB = databaseHelper.readFullTeacherFromDb(teacherId);
-
-        assertEquals("The teacher in the database should be equal to the updated teacher; ", teacherFromDB, updatedTeacher);
+        performPut(DEFAULT_PATH, teacher, BAD, status().isBadRequest());
     }
 
     @Test
-    public void updatingPartialTeacherWithValidData_shouldReturnOldTeacher() throws Exception {
-        FullTeacherDTO insertedTeacher = databaseHelper.insertFullTeacherIntoDb(makeFakeFullTeacher(TEACHER_SEED).withoutId());
-        int teacherId = insertedTeacher.getId();
+    public void insertingTeacherWithDuplicateEmail_shouldReturn409() throws Exception {
+        FullTeacherDTO teacher = FullTeacherDTO.stepBuilder()
+                .paymentDay(Fake.day(NEW_TEACHER_ID))
+                .hourlyWage(Fake.wage(NEW_TEACHER_ID))
+                .academicWage(Fake.wage(NEW_TEACHER_ID))
+                .name(Fake.name(NEW_TEACHER_ID))
+                .surname(Fake.surname(NEW_TEACHER_ID))
+                .phone(Fake.phone(NEW_TEACHER_ID))
+                .city(Fake.city(NEW_TEACHER_ID))
+                .email(Fake.email(OLD_TEACHER_ID))
+                .languages(Fake.language(NEW_TEACHER_ID))
+                .build();
 
-        FullTeacherDTO partialUpdatedTeacher = makeFakeFullTeacher(teacherId, null, null, null,
-                fakeName(TEACHER_SEED + 1), null, fakePhone(TEACHER_SEED + 1),
-                null, null, null, null, null, (List<String>) null);
-
-        mockMvc.perform(post("/timestar/api/v2/teacher")
-                .contentType(APPLICATION_JSON_UTF8)
-                .header("Authorization", authHeader())
-                .content(convertObjectToJsonBytes(partialUpdatedTeacher)))
-                .andDo(print())
-                .andExpect(status().isOk());
-
-        FullTeacherDTO teacherFromDB = databaseHelper.readFullTeacherFromDb(teacherId);
-        FullTeacherDTO updatedTeacher = makeFakeFullTeacher(teacherId, insertedTeacher.getPaymentDay(),
-                insertedTeacher.getHourlyWage(), insertedTeacher.getAcademicWage(),
-                partialUpdatedTeacher.getName(), insertedTeacher.getSurname(), partialUpdatedTeacher.getPhone(),
-                insertedTeacher.getCity(), insertedTeacher.getEmail(), insertedTeacher.getPicture(),
-                insertedTeacher.getDocument(), insertedTeacher.getComment(), insertedTeacher.getLanguages());
-
-        assertEquals("The teacher in the database should be equal to the updated teacher; ", teacherFromDB, updatedTeacher);
+        performPut(DEFAULT_PATH, teacher, BAD, status().isConflict());
     }
 
     @Test
-    public void updatingTeacherWithInvalidId_shouldReturnBadRequest() throws Exception {
-        FullTeacherDTO validTeacher = makeFakeFullTeacher(TEACHER_SEED);
+    public void updatingTeacherWithId_shouldUpdateTeacher() throws Exception {
+        FullTeacherDTO teacher = Fake.teacher(NEW_TEACHER_ID).withId(OLD_TEACHER_ID);
 
-        String json = convertObjectToString(validTeacher);
-        String invalidJson = replace(json, "id", -1);
+        performPost(DEFAULT_PATH, teacher, OK_NO_BODY);
 
-        mockMvc.perform(post("/timestar/api/v2/teacher")
-                .contentType(APPLICATION_JSON_UTF8)
-                .header("Authorization", authHeader())
-                .content(invalidJson))
-                .andDo(print())
-                .andExpect(status().isBadRequest());
+        assertInDatabase(teacher);
     }
 
     @Test
-    public void deletingTeacherWithValidId_shouldReturnDeletedTeacher() throws Exception {
-        FullTeacherDTO fullTeacher = makeFakeFullTeacher(TEACHER_SEED).withoutId();
-        FullTeacherDTO insertedTeacher = databaseHelper.insertFullTeacherIntoDb(fullTeacher);
-        databaseHelper.insertAccountIntoDb(fullTeacher);
-        int teacherId = insertedTeacher.getId();
+    public void updatingTeacherWithIdAndNameOnly_shouldUpdateTeacher() throws Exception {
+        FullTeacherDTO partialTeacher = FullTeacherDTO.builder()
+                .id(OLD_TEACHER_ID)
+                .name(Fake.name(NEW_TEACHER_ID))
+                .build();
 
-        mockMvc.perform(delete("/timestar/api/v2/teacher/{teacherId}", teacherId)
-                .contentType(APPLICATION_JSON_UTF8)
-                .header("Authorization", authHeader()))
-                .andDo(print())
-                .andExpect(status().isOk());
+        performPost(DEFAULT_PATH, partialTeacher, OK_NO_BODY);
 
-        FullTeacherDTO teacherFromDB = databaseHelper.readFullTeacherFromDb(teacherId);
+        FullTeacherDTO beforeUpdate = Fake.teacher(OLD_TEACHER_ID);
+        FullTeacherDTO afterUpdate = FullTeacherDTO.stepBuilder()
+                .paymentDay(beforeUpdate.getPaymentDay())
+                .hourlyWage(beforeUpdate.getHourlyWage())
+                .academicWage(beforeUpdate.getAcademicWage())
+                .name(partialTeacher.getName())
+                .surname(beforeUpdate.getSurname())
+                .phone(beforeUpdate.getPhone())
+                .city(beforeUpdate.getCity())
+                .email(beforeUpdate.getEmail())
+                .languages(beforeUpdate.getLanguages())
+                .id(partialTeacher.getId())
+                .picture(beforeUpdate.getPicture())
+                .document(beforeUpdate.getDocument())
+                .comment(beforeUpdate.getComment())
+                .build();
 
-        assertNull("The teacher from the database should be equal null, since it was deleted; ", teacherFromDB);
+        assertInDatabase(afterUpdate);
     }
 
     @Test
-    public void readingAllTeachers_shouldReturnListOfTeachers() throws Exception {
-        List<FullTeacherDTO> allTeachers = makeSomeFakes(2, FakeUtils::makeFakeFullTeacher).stream()
-                .map(FullTeacherDTO::withoutId)
-                .map(databaseHelper::insertFullTeacherIntoDb)
-                .collect(Collectors.toList());
+    public void updatingTeacherWithoutId_shouldReturn400() throws Exception {
+        FullTeacherDTO teacher = Fake.teacher(NEW_TEACHER_ID).withoutId();
 
-        MvcResult result = mockMvc.perform(get("/timestar/api/v2/teacher")
-                .contentType(APPLICATION_JSON_UTF8)
-                .header("Authorization", authHeader()))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(APPLICATION_JSON_UTF8))
-                .andReturn();
-
-        List<FullTeacherDTO> returnedTeachers = fromResponse(result, LIST_OF_TEACHERS);
-
-        List<FullTeacherDTO> teachersFromDb = allTeachers.stream()
-                .mapToInt(FullTeacherDTO::getId)
-                .mapToObj(databaseHelper::readFullTeacherFromDb)
-                .collect(Collectors.toList());
-
-        assertEquals("The read teachers should be equal to original teachers; ", returnedTeachers, allTeachers);
-        assertEquals("The teachers in the database should be equal to the read teachers; ", teachersFromDb, returnedTeachers);
+        performPost(DEFAULT_PATH, teacher, BAD, status().isBadRequest());
     }
 
     @Test
-    public void countingAllTeachers_shouldReturnCount() throws Exception {
-        List<FullTeacherDTO> allTeachers = makeSomeFakes(2, FakeUtils::makeFakeFullTeacher).stream()
-                .map(FullTeacherDTO::withoutId)
-                .map(databaseHelper::insertFullTeacherIntoDb)
-                .collect(Collectors.toList());
+    public void updatingTeacherWithOnlyId_shouldReturn400() throws Exception {
+        FullTeacherDTO teacher = FullTeacherDTO.builder()
+                .id(OLD_TEACHER_ID)
+                .build();
 
-        MvcResult result = mockMvc.perform(get("/timestar/api/v2/teacher/count")
-                .contentType(APPLICATION_JSON_UTF8)
-                .header("Authorization", authHeader()))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(APPLICATION_JSON_UTF8))
-                .andReturn();
+        performPost(DEFAULT_PATH, teacher, BAD, status().isBadRequest());
 
-        int returnedCount = fromResponse(result, Integer.class);
+        assertInDatabase(Fake.teacher(OLD_TEACHER_ID));
+    }
 
-        assertEquals("The amount of read teachers should be equal to original amount of teachers; ", returnedCount, allTeachers.size());
+    @Test
+    public void updatingTeacherWithNonExistentId_shouldReturn404() throws Exception {
+        FullTeacherDTO teacher = Fake.teacher(NEW_TEACHER_ID);
 
-        long amountOfTeachersInDb = allTeachers.stream()
-                .mapToInt(FullTeacherDTO::getId)
-                .mapToObj(databaseHelper::readFullTeacherFromDb)
-                .filter(teacher -> teacher != null)
-                .count();
+        performPost(DEFAULT_PATH, teacher, BAD, status().isNotFound());
 
-        assertEquals("The amount of teachers in the database should be equal to the amount of read teachers; ", amountOfTeachersInDb, returnedCount);
+        assertNotInDatabase(teacher);
+    }
+
+    @Test
+    public void updatingTeacherWithDuplicateEmail_shouldReturn409() throws Exception {
+        FullTeacherDTO teacher = FullTeacherDTO.stepBuilder()
+                .paymentDay(Fake.day(NEW_TEACHER_ID))
+                .hourlyWage(Fake.wage(NEW_TEACHER_ID))
+                .academicWage(Fake.wage(NEW_TEACHER_ID))
+                .name(Fake.name(NEW_TEACHER_ID))
+                .surname(Fake.surname(NEW_TEACHER_ID))
+                .phone(Fake.phone(NEW_TEACHER_ID))
+                .city(Fake.city(NEW_TEACHER_ID))
+                .email(Fake.email(OLD_TEACHER_ID))
+                .languages(Fake.language(NEW_TEACHER_ID))
+                .id(EXTRA_TEACHER_ID)
+                .build();
+
+        performPost(DEFAULT_PATH, teacher, BAD, status().isConflict());
+
+        assertInDatabase(Fake.teacher(EXTRA_TEACHER_ID));
+    }
+
+    @Test
+    public void deletingTeacherById_shouldDeleteTeacher() throws Exception {
+        FullTeacherDTO teacher = db.insertFullTeacher(Fake.teacher(NEW_TEACHER_ID));
+        db.insertValidAccount(teacher);
+
+        performDelete(DEFAULT_PATH + teacher.getId(), OK_NO_BODY);
+
+        assertNotInDatabase(teacher);
+    }
+
+    @Test
+    public void deletingTeacherByStillUsedId_shouldReturn400() throws Exception {
+        performDelete(DEFAULT_PATH + OLD_TEACHER_ID, BAD, status().isBadRequest());
+
+        assertInDatabase(OLD_TEACHER_ID);
+    }
+
+    @Test
+    public void deletingTeacherByNonExistentId_shouldReturn404() throws Exception {
+        performDelete(DEFAULT_PATH + NEW_TEACHER_ID, BAD, status().isNotFound());
+
+        assertNotInDatabase(NEW_TEACHER_ID);
+    }
+
+    @Test
+    public void readingTeacherById_shouldReturnTeacher() throws Exception {
+        FullTeacherDTO teacher = performGet(DEFAULT_PATH + OLD_TEACHER_ID, OK)
+                .map(Unchecked.function(this::readTeacher))
+                .orElseThrow(() -> new Exception("Successful read should return a teacher!"));
+
+        assertInDatabase(teacher);
+    }
+
+    @Test
+    public void readingTeacherByNonExistentId_shouldReturn404() throws Exception {
+        performGet(DEFAULT_PATH + NEW_TEACHER_ID, BAD, status().isNotFound());
+
+        assertNotInDatabase(NEW_TEACHER_ID);
+    }
+
+    @Test
+    public void readingAllTeachers_shouldReturnTeachers() throws Exception {
+        List<FullTeacherDTO> teachers = performGet(DEFAULT_PATH, OK)
+                .map(Unchecked.function(this::readTeachers))
+                .orElseThrow(() -> new AssertionError("Should return empty list instead of null"));
+
+        assertEquals("When returning all, 2 objects should be returned", 2, teachers.size());
+        teachers.forEach(this::assertInDatabase);
+    }
+
+    @Test
+    public void countingAllTeachers_shouldReturn2() throws Exception {
+        int count = performGet(DEFAULT_PATH + "count", OK)
+                .map(Unchecked.function(this::readCount))
+                .orElseThrow(() -> new Exception("Successful read should return an integer!"));
+
+        assertEquals("When counting all, 2 should be returned", 2, count);
     }
 
     // PRIVATE
 
+    private FullTeacherDTO readTeacher(MvcResult result) throws IOException {
+        return fromResponse(result, FullTeacherDTO.class);
+    }
+
+    private List<FullTeacherDTO> readTeachers(MvcResult result) throws IOException {
+        return fromResponse(result, LIST_OF_TEACHERS);
+    }
+
+    private void assertNotInDatabase(FullTeacherDTO teacher) {
+        assertNotInDatabase(DB::readFullTeacher, teacher.getId());
+    }
+
+    private void assertNotInDatabase(int teacherId) {
+        assertNotInDatabase(DB::readFullTeacher, teacherId);
+    }
+
+    private void assertInDatabase(FullTeacherDTO teacher) {
+        assertInDatabase(DB::readFullTeacher, FullTeacherDTO::getId, teacher);
+    }
+
+    private void assertInDatabase(int teacherId) {
+        assertInDatabase(DB::readFullTeacher, teacherId);
+    }
+
+    private static final String DEFAULT_PATH = "/timestar/api/v2/teacher/";
+    private static final int EXTRA_TEACHER_ID = 2;
+
     private static final TypeReference<List<FullTeacherDTO>> LIST_OF_TEACHERS = new TypeReference<List<FullTeacherDTO>>() {};
-    private static final int TEACHER_SEED = 1;*/
 
 }
