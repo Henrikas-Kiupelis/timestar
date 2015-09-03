d5277093953deba9eb227f8ab65dcdcbfce6b0d0
Goodlike <henrisssss@hotmail.com>
2015-09-03 10:54:14 +0300

    Updated ValidCustomerController tests to use the new methods

--- src/test/java/com/superum/api/customer/ValidCustomerControllerIT.java
+++ src/test/java/com/superum/api/customer/ValidCustomerControllerIT.java
@@ -1,34 +1,247 @@
 package com.superum.api.customer;
 
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.superum.utils.FakeUtils;
-import env.IntegrationTestEnvironment;
+import com.superum.env.IntegrationTestEnvironment;
+import com.superum.helper.DB;
+import com.superum.helper.Fake;
+import org.jooq.lambda.Unchecked;
 import org.junit.Test;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.function.BiPredicate;
 
-import static com.superum.utils.FakeFieldUtils.fakeName;
-import static com.superum.utils.FakeFieldUtils.fakePhone;
-import static com.superum.utils.FakeUtils.makeFakeValidCustomer;
-import static com.superum.utils.FakeUtils.makeSomeFakes;
-import static com.superum.utils.JsonUtils.*;
+import static com.superum.helper.ResultVariation.*;
 import static com.superum.utils.MockMvcUtils.fromResponse;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @TransactionConfiguration(defaultRollback = true)
 @Transactional
-public class FullCustomerControllerTestsIT extends IntegrationTestEnvironment {
+public class ValidCustomerControllerIT extends IntegrationTestEnvironment {
 
     @Test
+    public void insertingCustomerWithoutId_shouldCreateNewCustomer() throws Exception {
+        ValidCustomerDTO customer = Fake.customer(NEW_CUSTOMER_ID).withoutId();
+
+        ValidCustomerDTO insertedCustomer = performPut(DEFAULT_PATH, customer, OK)
+                .map(Unchecked.function(this::readCustomer))
+                .orElseThrow(() -> new Exception("Successful insertion should return a customer!"));
+
+        assertEquals("Inserted customer should be equal to the original (except id)",
+                customer.withId(insertedCustomer.getId()), insertedCustomer);
+
+        assertInDatabase(insertedCustomer);
+    }
+
+    @Test
+    public void insertingCustomerWithId_shouldReturn400() throws Exception {
+        ValidCustomerDTO customer = Fake.customer(NEW_CUSTOMER_ID);
+
+        performPut(DEFAULT_PATH, customer, BAD, status().isBadRequest());
+
+        assertNotInDatabase(customer);
+    }
+
+    @Test
+    public void insertingCustomerWithoutName_shouldReturn400() throws Exception {
+        ValidCustomerDTO customer = ValidCustomerDTO.builder()
+                .startDate(Fake.localDate(NEW_CUSTOMER_ID))
+                .phone(Fake.phone(NEW_CUSTOMER_ID))
+                .website(Fake.website(NEW_CUSTOMER_ID))
+                .build();
+
+        performPut(DEFAULT_PATH, customer, BAD, status().isBadRequest());
+    }
+
+    @Test
+    public void updatingCustomerWithId_shouldUpdateCustomer() throws Exception {
+        ValidCustomerDTO customer = Fake.customer(NEW_CUSTOMER_ID).withId(OLD_CUSTOMER_ID);
+
+        performPost(DEFAULT_PATH, customer, OK_NO_BODY);
+
+        assertInDatabase(customer);
+    }
+
+    @Test
+    public void updatingCustomerWithIdAndNameOnly_shouldUpdateCustomer() throws Exception {
+        ValidCustomerDTO partialCustomer = ValidCustomerDTO.builder()
+                .id(OLD_CUSTOMER_ID)
+                .name(Fake.name(NEW_STUDENT_ID))
+                .build();
+
+        performPost(DEFAULT_PATH, partialCustomer, OK_NO_BODY);
+
+        ValidCustomerDTO beforeUpdate = Fake.customer(OLD_CUSTOMER_ID);
+        ValidCustomerDTO afterUpdate = ValidCustomerDTO.builder()
+                .id(partialCustomer.getId())
+                .startDate(beforeUpdate.getStartDate())
+                .name(partialCustomer.getName())
+                .phone(beforeUpdate.getPhone())
+                .website(beforeUpdate.getWebsite())
+                .picture(beforeUpdate.getPicture())
+                .comment(beforeUpdate.getComment())
+                .build();
+
+        assertInDatabase(afterUpdate);
+    }
+
+    @Test
+    public void updatingCustomerWithoutId_shouldReturn400() throws Exception {
+        ValidCustomerDTO customer = Fake.customer(NEW_CUSTOMER_ID).withoutId();
+
+        performPost(DEFAULT_PATH, customer, BAD, status().isBadRequest());
+    }
+
+    @Test
+    public void updatingCustomerWithOnlyId_shouldReturn400() throws Exception {
+        ValidCustomerDTO customer = ValidCustomerDTO.builder()
+                .id(OLD_CUSTOMER_ID)
+                .build();
+
+        performPost(DEFAULT_PATH, customer, BAD, status().isBadRequest());
+
+        assertInDatabase(Fake.customer(OLD_CUSTOMER_ID));
+    }
+
+    @Test
+    public void updatingCustomerWithNonExistentId_shouldReturn404() throws Exception {
+        ValidCustomerDTO customer = Fake.customer(NEW_CUSTOMER_ID);
+
+        performPost(DEFAULT_PATH, customer, BAD, status().isNotFound());
+
+        assertNotInDatabase(customer);
+    }
+
+    @Test
+    public void deletingCustomerById_shouldDeleteCustomer() throws Exception {
+        ValidCustomerDTO customer = db.insertValidCustomer(Fake.customer(NEW_CUSTOMER_ID));
+
+        performDelete(DEFAULT_PATH + NEW_CUSTOMER_ID, OK_NO_BODY);
+
+        assertNotInDatabase(customer);
+    }
+
+    @Test
+    public void deletingCustomerByStillUsedId_shouldReturn400() throws Exception {
+        performDelete(DEFAULT_PATH + OLD_CUSTOMER_ID, BAD, status().isBadRequest());
+
+        assertInDatabase(OLD_CUSTOMER_ID);
+    }
+
+    @Test
+    public void deletingCustomerByNonExistentId_shouldReturn404() throws Exception {
+        performDelete(DEFAULT_PATH + NEW_CUSTOMER_ID, BAD, status().isNotFound());
+
+        assertNotInDatabase(NEW_CUSTOMER_ID);
+    }
+
+    @Test
+    public void readingCustomerById_shouldReturnCustomer() throws Exception {
+        ValidCustomerDTO customer = performGet(DEFAULT_PATH + OLD_CUSTOMER_ID, OK)
+                .map(Unchecked.function(this::readCustomer))
+                .orElseThrow(() -> new Exception("Successful read should return a customer!"));
+
+        assertInDatabase(customer);
+    }
+
+    @Test
+    public void readingCustomerByNonExistentId_shouldReturn404() throws Exception {
+        performGet(DEFAULT_PATH + NEW_CUSTOMER_ID, BAD, status().isNotFound());
+
+        assertNotInDatabase(NEW_CUSTOMER_ID);
+    }
+
+    @Test
+    public void readingAllCustomers_shouldReturnCustomers() throws Exception {
+        List<ValidCustomerDTO> customers = performGet(DEFAULT_PATH, OK)
+                .map(Unchecked.function(this::readCustomers))
+                .orElseThrow(() -> new AssertionError("Should return empty list instead of null"));
+
+        assertEquals("When returning all, 2 objects should be returned", 2, customers.size());
+        customers.forEach(this::assertInDatabase);
+    }
+
+    @Test
+    public void countingAllCustomers_shouldReturnCountOf2() throws Exception {
+        int count = performGet(DEFAULT_PATH + "count", OK)
+                .map(Unchecked.function(this::readCount))
+                .orElseThrow(() -> new Exception("Successful read should return an integer!"));
+
+        assertEquals("When counting all, 2 should be returned", 2, count);
+    }
+
+    @Test
+    public void readingCustomersForTeacher_shouldReturnCustomers() throws Exception {
+        List<ValidCustomerDTO> customers = performGet(DEFAULT_PATH + "teacher/" + OLD_TEACHER_ID, OK)
+                .map(Unchecked.function(this::readCustomers))
+                .orElseThrow(() -> new AssertionError("Should return empty list instead of null"));
+
+        assertEquals("When returning for something, 1 object should be returned", 1, customers.size());
+        customers.forEach(this::assertInDatabase);
+    }
+
+    @Test
+    public void readingCustomersForNonExistentTeacher_shouldReturn404() throws Exception {
+        performGet(DEFAULT_PATH + "teacher/" + NEW_TEACHER_ID, BAD, status().isNotFound());
+
+        assertNotInDatabase(NEW_TEACHER_ID);
+    }
+
+    @Test
+    public void countingCustomersForTeacher_shouldReturnCountOf1() throws Exception {
+        int count = performGet(DEFAULT_PATH + "teacher/" + OLD_TEACHER_ID + "/count", OK)
+                .map(Unchecked.function(this::readCount))
+                .orElseThrow(() -> new Exception("Successful read should return an integer!"));
+
+        assertEquals("When counting for something, 1 should be returned", 1, count);
+    }
+
+    @Test
+    public void countingCustomersForNonExistentTeacher_shouldReturn404() throws Exception {
+        performGet(DEFAULT_PATH + "teacher/" + NEW_TEACHER_ID + "/count", BAD, status().isNotFound());
+
+        assertNotInDatabase(NEW_TEACHER_ID);
+    }
+
+    // PRIVATE
+
+    private ValidCustomerDTO readCustomer(MvcResult result) throws IOException {
+        return fromResponse(result, ValidCustomerDTO.class);
+    }
+
+    private List<ValidCustomerDTO> readCustomers(MvcResult result) throws IOException {
+        return fromResponse(result, LIST_OF_CUSTOMERS);
+    }
+
+    private void assertNotInDatabase(ValidCustomerDTO customer) {
+        assertNotInDatabase(DB::readValidCustomer, customer.getId());
+    }
+
+    private void assertNotInDatabase(int customerId) {
+        assertNotInDatabase(DB::readValidCustomer, customerId);
+    }
+
+    private void assertInDatabase(ValidCustomerDTO customer) {
+        assertInDatabase(DB::readValidCustomer, ValidCustomerDTO::getId, customer);
+    }
+
+    private void assertInDatabase(ValidCustomerDTO customer, BiPredicate<ValidCustomerDTO, ValidCustomerDTO> customEqualityCheck) {
+        assertInDatabase(DB::readValidCustomer, ValidCustomerDTO::getId, customer, customEqualityCheck);
+    }
+
+    private void assertInDatabase(int customerId) {
+        assertInDatabase(DB::readValidCustomer, customerId);
+    }
+
+    private static final String DEFAULT_PATH = "/timestar/api/v2/customer/";
+
+    private static final TypeReference<List<ValidCustomerDTO>> LIST_OF_CUSTOMERS = new TypeReference<List<ValidCustomerDTO>>() {};
+
+   /* @Test
     public void insertingCustomerWithoutId_shouldCreateNewCustomer() throws Exception {
         ValidCustomerDTO customer = makeFakeValidCustomer(CUSTOMER_SEED).withoutId();
 
@@ -47,7 +260,7 @@ public class FullCustomerControllerTestsIT extends IntegrationTestEnvironment {
 
         assertEquals("The returned customer should be equal to original customer, except for id field; ", returnedCustomer, customer);
 
-        ValidCustomerDTO customerFromDB = databaseHelper.readFullCustomerFromDb(customerId);
+        ValidCustomerDTO customerFromDB = databaseHelper.readFullCustomer(customerId);
 
         assertEquals("The customer in the database should be equal to the returned customer; ", customerFromDB, returnedCustomer);
     }
@@ -69,7 +282,7 @@ public class FullCustomerControllerTestsIT extends IntegrationTestEnvironment {
 
         assertEquals("The read customer should be equal to original customer; ", returnedCustomer, insertedCustomer);
 
-        ValidCustomerDTO customerFromDB = databaseHelper.readFullCustomerFromDb(customerId);
+        ValidCustomerDTO customerFromDB = databaseHelper.readFullCustomer(customerId);
 
         assertEquals("The customer in the database should be equal to the returned customer; ", customerFromDB, returnedCustomer);
     }
@@ -88,7 +301,7 @@ public class FullCustomerControllerTestsIT extends IntegrationTestEnvironment {
                 .andDo(print())
                 .andExpect(status().isOk());
 
-        ValidCustomerDTO customerFromDB = databaseHelper.readFullCustomerFromDb(customerId);
+        ValidCustomerDTO customerFromDB = databaseHelper.readFullCustomer(customerId);
 
         assertEquals("The customer in the database should be equal to the updated customer; ", customerFromDB, updatedCustomer);
     }
@@ -109,7 +322,7 @@ public class FullCustomerControllerTestsIT extends IntegrationTestEnvironment {
                 .andDo(print())
                 .andExpect(status().isOk());
 
-        ValidCustomerDTO customerFromDB = databaseHelper.readFullCustomerFromDb(customerId);
+        ValidCustomerDTO customerFromDB = databaseHelper.readFullCustomer(customerId);
         ValidCustomerDTO updatedCustomer = makeFakeValidCustomer(customerId, insertedCustomer.getStartDate(),
                 partialUpdatedCustomer.getName(), partialUpdatedCustomer.getPhone(),
                 insertedCustomer.getWebsite(), insertedCustomer.getPicture(), insertedCustomer.getComment());
@@ -143,10 +356,10 @@ public class FullCustomerControllerTestsIT extends IntegrationTestEnvironment {
                 .andDo(print())
                 .andExpect(status().isOk());
 
-        ValidCustomerDTO customerFromDB = databaseHelper.readFullCustomerFromDb(customerId);
+        ValidCustomerDTO customerFromDB = databaseHelper.readFullCustomer(customerId);
 
         assertNull("The customer from the database should be equal null, since it was deleted; ", customerFromDB);
-    }
+    }*/
 
     @Test
     public void readingCustomerForTeacherWithValidId_shouldReturnListOfCustomers() throws Exception {
@@ -176,13 +389,13 @@ public class FullCustomerControllerTestsIT extends IntegrationTestEnvironment {
 
         assertEquals("The read customers should be equal to original customers; ", returnedCustomers, validCustomers);
 
-        ValidCustomerDTO customerFromDB = databaseHelper.readFullCustomerFromDb(customerId);
-        List<ValidCustomerDTO> customersFromDb = Collections.singletonList(customerFromDB);
+        ValidCustomerDTO customerFromDB = databaseHelper.readFullCustomer(customerId);
+        List<ValidCustomerDTO> customers = Collections.singletonList(customerFromDB);
 
-        assertEquals("The customers in the database should be equal to the read customers; ", customersFromDb, returnedCustomers);*/
+        assertEquals("The customers in the database should be equal to the read customers; ", customers, returnedCustomers);*/
     }
 
-    @Test
+    /*@Test
     public void readingAllCustomers_shouldReturnListOfCustomers() throws Exception {
         List<ValidCustomerDTO> allCustomers = makeSomeFakes(2, FakeUtils::makeFakeValidCustomer).stream()
                 .map(ValidCustomerDTO::withoutId)
@@ -201,13 +414,13 @@ public class FullCustomerControllerTestsIT extends IntegrationTestEnvironment {
 
         assertEquals("The read customers should be equal to original customers; ", returnedCustomers, allCustomers);
 
-        List<ValidCustomerDTO> customersFromDb = allCustomers.stream()
+        List<ValidCustomerDTO> customers = allCustomers.stream()
                 .mapToInt(ValidCustomerDTO::getId)
-                .mapToObj(databaseHelper::readFullCustomerFromDb)
+                .mapToObj(databaseHelper::readFullCustomer)
                 .collect(Collectors.toList());
 
-        assertEquals("The customers in the database should be equal to the read customers; ", customersFromDb, returnedCustomers);
-    }
+        assertEquals("The customers in the database should be equal to the read customers; ", customers, returnedCustomers);
+    }*/
 
     @Test
     public void countingCustomersForTeacherWithValidId_shouldReturnCount() throws Exception {
@@ -237,13 +450,13 @@ public class FullCustomerControllerTestsIT extends IntegrationTestEnvironment {
 
         assertEquals("The amount of read customers should be equal to original amount of customers; ", returnedCount, validCustomers.size());
 
-        ValidCustomerDTO customerFromDB = databaseHelper.readFullCustomerFromDb(customerId);
-        List<ValidCustomerDTO> customersFromDb = Collections.singletonList(customerFromDB);
+        ValidCustomerDTO customerFromDB = databaseHelper.readFullCustomer(customerId);
+        List<ValidCustomerDTO> customers = Collections.singletonList(customerFromDB);
 
-        assertEquals("The amount of customers from the database should be equal to the amount of read customers; ", customersFromDb.size(), returnedCount);*/
+        assertEquals("The amount of customers from the database should be equal to the amount of read customers; ", customers.size(), returnedCount);*/
     }
 
-    @Test
+    /*@Test
     public void countingAllCustomers_shouldReturnCount() throws Exception {
         List<ValidCustomerDTO> allCustomers = makeSomeFakes(2, FakeUtils::makeFakeValidCustomer).stream()
                 .map(ValidCustomerDTO::withoutId)
@@ -264,7 +477,7 @@ public class FullCustomerControllerTestsIT extends IntegrationTestEnvironment {
 
         long amountOfCustomersInDb = allCustomers.stream()
                 .mapToInt(ValidCustomerDTO::getId)
-                .mapToObj(databaseHelper::readFullCustomerFromDb)
+                .mapToObj(databaseHelper::readFullCustomer)
                 .filter(customer -> customer != null)
                 .count();
 
@@ -274,6 +487,6 @@ public class FullCustomerControllerTestsIT extends IntegrationTestEnvironment {
     // PRIVATE
 
     private static final TypeReference<List<ValidCustomerDTO>> LIST_OF_CUSTOMERS = new TypeReference<List<ValidCustomerDTO>>() {};
-    private static final int CUSTOMER_SEED = 1;
+    private static final int CUSTOMER_SEED = 1;*/
 
 }
