31fdba4a9f9c9651b8b0f3f83c7571ae121438da
Goodlike <henrisssss@hotmail.com>
2015-11-06 15:56:41 +0200

    Correctly implemented CORS

--- src/main/java/com/superum/config/CORSFilter.java
+++ src/main/java/com/superum/config/CORSFilter.java
@@ -6,18 +6,21 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 public class CORSFilter implements Filter {
 
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) req;
-        if ("XMLHttpRequest".equals(request.getHeader("X-Requested-With"))) {
+        String origin = request.getHeader("origin");
+        if (allowedOrigin(origin)) {
             HttpServletResponse response = (HttpServletResponse) res;
-            response.setHeader("Access-Control-Allow-Origin", "*");
-            response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
-            response.setHeader("Access-Control-Max-Age", "3600");
-            response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+            response.setHeader("Access-Control-Allow-Origin", origin);
+            response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS, PATCH");
+            response.setHeader("Access-Control-Max-Age", "604800");
+            response.setHeader("Access-Control-Allow-Headers", "Authorization");
         }
         chain.doFilter(req, res);
     }
@@ -25,5 +28,17 @@ public class CORSFilter implements Filter {
     public void init(FilterConfig filterConfig) {}
 
     public void destroy() {}
+
+    private final List<String> acceptedOrigins = Arrays.asList(
+            "http://localhost",
+            "https://localhost",
+            "http://timestar.semwin.lt",
+            "https://timestar.semwin.lt");
+
+    private boolean allowedOrigin(String origin) {
+        if (origin.endsWith("/"))
+            origin = origin.substring(0, origin.length() - 1);
+        return acceptedOrigins.contains(origin);
+    }
 
 }
